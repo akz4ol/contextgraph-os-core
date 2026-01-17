bbc07f7db36040421ff78b2c49b97d739e8e0745
anup.kumar3 <blog.mot2gmob@gmail.com>
2026-01-17 19:52:54 +0530

    fix: convert jest.config to .cjs for Node 18 compatibility

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,6 +1,5 @@
-import type { Config } from 'jest';
-
-const config: Config = {
+/** @type {import('jest').Config} */
+const config = {
   preset: 'ts-jest/presets/default-esm',
   testEnvironment: 'node',
   roots: ['<rootDir>/tests'],
@@ -39,4 +38,4 @@ const config: Config = {
   restoreMocks: true,
 };
 
-export default config;
+module.exports = config;
